6ffc88c22dc4e4c6f0917eb974866029f4f8c572
simonsun26 <sunang8888@outlook.com>
2022-06-03 01:04:41 +0200

    new3

--- src/Taxi.java
+++ src/Taxi.java
@@ -21,23 +21,27 @@ public class Taxi {
     }
 
     public String toString() {
-        String driver_mess = "This is the Taxi of " + driver.toString() + ". ";
         String pass_info = "";
-
+        
         if (passengers[0] == null) {
             pass_info = "nobody";
         } else {
-            for (int i = 0; i < passengers.length; i++) {
+            int n = 0;
+            for (int i = passengers.length - 1; i >= 0; i--) {
                 if (passengers[i] != null) {
-                    if (i != 0) {
-                        pass_info = pass_info + ", ";
+                    if (n == 1) {
+                        pass_info = " and " + pass_info;
                     }
-                    pass_info = pass_info + passengers[i].toString();
+                    if (n > 1) {
+                        pass_info = ", " + pass_info;
+                    }
+                    pass_info = passengers[i].toString() + pass_info;
+                    n ++;
                 }
             }
         }
         
-        return driver_mess + "He takes " + pass_info + " along.";
+        return "This is the Taxi of " + driver.toString() + ". He takes " + pass_info + " along.";
     }
 
     public static void main(String[] args) {
@@ -45,6 +49,7 @@ public class Taxi {
     onetaxi.driver = new Human("KK", "Alow");
     onetaxi.add(new Human("Pul", "Jin"));
     onetaxi.add(new Human("Ben", "Dog"));
+    onetaxi.add(new Human("Ali", "Toa"));
     // System.out.println(onetaxi.getDriverName());
     // System.out.println(onetaxi.passengers[0].toString());
     System.out.println(onetaxi.toString());
